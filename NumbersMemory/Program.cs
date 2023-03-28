69ae4a8266fec5732a11ab30a1b0b28afc2bed12
Gramblicka Matus <Gmt@int.kistler.com>
2023-03-28 13:35:41 +0200

    initial

---
+++ NumbersMemory/Program.cs
@@ -0,0 +1,48 @@
+﻿while (true)
+{
+    var rnd = new Random();
+    var rndInt = rnd.Next(1000000, 9999999);
+    Console.WriteLine(rndInt);
+
+    Console.WriteLine("\n\nPress any key for continue, key 'q' for exit");
+
+    var anyKey = Console.ReadKey();
+
+    if (anyKey.KeyChar == 'q')
+    {
+        Console.Clear();
+        break;
+    }
+    Console.Clear();
+
+    var input = Console.ReadLine();
+    var success = int.TryParse(input, out var number);
+    if (!success)
+    {
+        Console.Clear();
+        Console.WriteLine("Invalid input, press any key for continue, key 'q' for exit");
+        anyKey = Console.ReadKey();
+
+        if (anyKey.KeyChar == 'q')
+        {
+            Console.Clear();
+            break;
+        }
+    }
+    else
+    {
+        if (number != rndInt)
+        {
+            Console.WriteLine("Invalid input, press any key for continue, key 'q' for exit");
+            anyKey = Console.ReadKey();
+
+            if (anyKey.KeyChar == 'q')
+            {
+                Console.Clear();
+                break;
+            }
+        }
+    }
+
+    Console.Clear();
+}
